f5f8eb51675bc38a25dc7dffef7c86772b7e9735
John Alapatt <John.Alapatt@smu.ca>
2023-04-06 18:47:30 -0300

    Updated Code

--- FileA.py
+++ FileA.py
@@ -2,6 +2,8 @@ import streamlit as st
 from joblib import load
 from skimage import io
 from sklearn.datasets import load_iris
+from PIL import Image
+
 
 st.title("Iris Flower Species Predictor")
 st.write("-----------------------------------")
@@ -21,17 +23,16 @@ pe_w = st.slider('petal width (cm)', min_value=0, max_value=10)
 prediction = clf.predict([[sp_l, sp_w, pe_l, pe_w]])
 prediction = LABELS[prediction[0]]
 
-iris = load_iris()
-# label_index = LABELS.index(prediction) 
-# images = iris.images[iris.target == label_index]
-LABELS = ['Setosa', 'Versicolor', 'Virginica']
-label_index = LABELS.index('Setosa')
-images = iris.images[iris.target == label_index]
+
+setosa= Image.open('setosa.png')
+versicolor= Image.open('versicolor.png')
+virginica = Image.open('virginica.png')
 
 
-st.write("The Species of Iris Flower that has been identified is:") 
-st.write(prediction)
-io.imshow(images[0])
-io.show()
-st.write("-----------------------------------")
+
+if st.button("Click Here to Classify"):
+	st.write("The Species of Iris Flower that has been identified is:") 
+	st.write(prediction)
+	st.image(setosa) if prediction == 0 else st.image(versicolor)  if prediction == 1 else st.image(virginica) 
+	st.write("-----------------------------------")
 		
